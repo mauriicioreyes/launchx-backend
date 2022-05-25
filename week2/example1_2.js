0cb65b01be3dbe38f7e2b710b0175b2a9be821d1
Mauricio Kraken <mauricioreyes.dev@gmail.com>
2022-05-25 18:03:59 -0500

    Agregando ejemplo 2: operadores, forEach para calcular una suma de una lista

---
+++ week2/example1_2.js
@@ -0,0 +1,8 @@
+// Ejemplo 2: forEach para calcular la suma de todos los elementos de una lista
+
+let sum = 0;
+const numbers = [1, 2, 3, 4, 5];
+numbers.forEach(num => sum += num)
+
+console.log("\nEJEMPLO 2: Cálculo de la suma de todos los elementos de la lista")
+console.log(sum)
