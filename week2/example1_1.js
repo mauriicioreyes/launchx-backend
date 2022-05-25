b76b97a58cc3d9d52d2a55c3f773b9e409483da6
Mauricio Kraken <mauricioreyes.dev@gmail.com>
2022-05-25 18:03:22 -0500

    Agregando ejemplo 1: operadores, forEach para imprimir elementos de una lista

---
+++ week2/example1_1.js
@@ -0,0 +1,7 @@
+// Ejemplo 1: forEach para imprimir elementos de una lista
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log("\EJEMPLO 1: Imprimiendo los elementos de una lista")
+
+numbers.forEach(num => console.log(num))
