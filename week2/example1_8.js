b008fbc5bc959038eecec4f57ac34c5f26de14cd
Mauricio Kraken <mauricioreyes.dev@gmail.com>
2022-05-26 19:44:56 -0500

    Agregando ejemplo 8: operadores, filtrar una lista por condicional

---
+++ week2/example1_8.js
@@ -0,0 +1,18 @@
+// Ejemplo 8: Filtrar una lista por condicional
+
+const scores = [
+    { name: 'A', score: 95 },
+    { name: 'L', score: 98 },
+    { name: 'M', score: 80 },
+    { name: 'E', score: 50 },
+    { name: 'M', score: 85 },
+    { name: 'J', score: 100 },
+]
+
+const scoresGreaterEighty = scores.filter((score) => 
+    score.score > 80
+)
+
+console.log("\nEjemplo 8: Filtrando elementos por score")
+console.log(scores)
+console.log(scoresGreaterEighty)
