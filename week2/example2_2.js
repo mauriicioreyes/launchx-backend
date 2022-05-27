b16e66898cdc05846f3e453cce67a8adecc39f75
Mauricio Kraken <mauricioreyes.dev@gmail.com>
2022-05-26 23:08:07 -0500

    Agregando ejemplo 2: clases y objetos. Crear un objeto a partir de una clase

---
+++ week2/example2_2.js
@@ -0,0 +1,10 @@
+// Ejemplo 2: Crear un objeto a partir de una clase
+
+class Pet {
+
+}
+
+const myPet1 = new Pet() // podemos crear muchos objetos de la clase Pet
+
+console.log("\nEjemplo 2: Crear un objeto a partir de una clase")
+console.log(myPet1)
