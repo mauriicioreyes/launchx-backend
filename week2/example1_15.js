b9d7b8dc799e8d9f85ea988ff0faaf44a1977693
Mauricio Kraken <mauricioreyes.dev@gmail.com>
2022-05-26 21:02:40 -0500

    Agregando ejemplo 15: operadores, uso de 'sort' para ordenar elementos

---
+++ week2/example1_15.js
@@ -0,0 +1,7 @@
+// Ejemplo 15: Uso de Sort para ordenar elementos
+
+const products = ['Milk', 'Eggs', 'Coffee', 'Sugar', 'Apples', 'Lemons', 'Bananas', 'Water']
+
+console.log("\nEjemplo 15: Elementos ordenados usando SORT")
+console.log(products)
+console.log(products.sort())
