705656e0ca2cf52ea12fd002c1f827731edeb87c
Mauricio Kraken <mauricioreyes.dev@gmail.com>
2022-05-27 14:39:45 -0500

    Elimina prueba de ejemplo y agrega test para crear un objeto pokemon

--- week2/4_modularizacion_clases/main.js
+++
@@ -1,9 +0,0 @@
-import Explorer from './explorer.js'
-import Viajero from './viajero.js'
-
-// Este es un ejemplo de como modularizar clases y usarlas mediante módulos
-
-const viajero1 = new Viajero("Mauricio", "mauriicioreyes", "Node JS", "Abril 2022")
-
-console.log("\nModularización de clases")
-console.log(viajero1)
--- week2/4_unit_tests/pokemon.test.js
+++ week2/4_unit_tests/pokemon.test.js
@@ -1,5 +1,6 @@
 import Pokemon from './pokemon'
 
-test('Test 1) Mi prueba', () => {
-    expect(4 + 2).toBe(60);
+test('1) Create a new object pokemon', () => {
+    const myPokemon = new Pokemon("Pikachu")
+    expect(myPokemon.name).toBe("Pikachu")
 })
