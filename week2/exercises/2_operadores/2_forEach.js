81a92e443155b6a137964b62d425bcca50636ad5
Mauricio Kraken <mauricioreyes.dev@gmail.com>
2022-06-07 20:12:10 -0500

    Repasando ejercicio 2, forEach para imprimir el stack de cada explorer

--- week2/exercises/2_operadores/2_forEach.js
+++ week2/exercises/2_operadores/2_forEach.js
@@ -1,6 +1,8 @@
-import { explorers } from './lista.js'
+import { explorers } from "./lista.js";
 
-const expl = explorers
+const expl = explorers;
+
+console.log("\nEjercicio 2: Imprimir el stack de cada explorer con forEach")
 
 expl.forEach((element) =>
     console.log(element.stack)
